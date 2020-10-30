5bd14a76a29b1169fbb9f6f0ff2edaa61bef63b5
jonacappelle <jonacappelle@hotmail.com>
2020-10-30 14:57:32 +0100

    Read multiple bytes + write single byte working

--- main.c
+++ main.c
@@ -19,6 +19,7 @@
 
 /* I2C address of IMU */
 #define ICM_20948_I2C_ADDRESS		0x69U
+#define ICM_20948_WHOAMI				0x00U
 
 /* Indicates if operation on TWI has ended. */
 static volatile bool m_xfer_done = false;
@@ -27,28 +28,7 @@ static volatile bool m_xfer_done = false;
 static const nrf_drv_twi_t m_twi = NRF_DRV_TWI_INSTANCE(TWI_INSTANCE_ID);
 
 /* Buffer for samples read from IMU */
-static uint8_t value[1];
-
-/**
- * @brief Function for setting active mode on MMA7660 accelerometer.
- */
-//void LM75B_set_mode(void)
-//{
-//    ret_code_t err_code;
-
-//    /* Writing to LM75B_REG_CONF "0" set temperature sensor in NORMAL mode. */
-//    uint8_t reg[2] = {LM75B_REG_CONF, NORMAL_MODE};
-//    err_code = nrf_drv_twi_tx(&m_twi, LM75B_ADDR, reg, sizeof(reg), false);
-//    APP_ERROR_CHECK(err_code);
-//    while (m_xfer_done == false);
-
-//    /* Writing to pointer byte. */
-//    reg[0] = LM75B_REG_TEMP;
-//    m_xfer_done = false;
-//    err_code = nrf_drv_twi_tx(&m_twi, LM75B_ADDR, reg, 1, false);
-//    APP_ERROR_CHECK(err_code);
-//    while (m_xfer_done == false);
-//}
+static uint8_t data[1];
 
 /**
  * @brief Function for handling data from temperature sensor.
@@ -70,7 +50,7 @@ void twi_handler(nrf_drv_twi_evt_t const * p_event, void * p_context)
         case NRF_DRV_TWI_EVT_DONE:
             if (p_event->xfer_desc.type == NRF_DRV_TWI_XFER_RX)
             {
-                data_handler(value[0]);
+                data_handler(data[0]);
             }
             m_xfer_done = true;
             break;
@@ -107,64 +87,62 @@ void twi_init (void)
     nrf_drv_twi_enable(&m_twi);
 }
 
-/**
- * @brief Function for reading data from temperature sensor.
- */
-//static void read_sensor_data()
-//{
-//    m_xfer_done = false;
-
-//    /* Read 1 byte from the specified address - skip 3 bits dedicated for fractional part of temperature. */
-//    ret_code_t err_code = nrf_drv_twi_rx(&m_twi, LM75B_ADDR, &m_sample, sizeof(m_sample));
-//    APP_ERROR_CHECK(err_code);
-//}
-
 
 //write byte to register
-void write_byte(const nrf_drv_twi_t *twi_handle, uint8_t address, uint8_t sub_address, uint8_t *data, bool stop)
+ret_code_t i2c_write_byte(const nrf_drv_twi_t *twi_handle, uint8_t address, uint8_t sub_address, uint8_t* data, bool stop)
 {   
 		ret_code_t err_code;
 		uint8_t data_write[2];
 		
 		data_write[0] = sub_address;
 		data_write[1] = *data;
-				
+	
+		m_xfer_done = false;
+	
 		err_code = nrf_drv_twi_tx(twi_handle, address, data_write, sizeof(data_write), stop);  
 		APP_ERROR_CHECK(err_code);
-}
-
-//reading byte or bytes from register            
-ret_code_t write_forread_byte(const nrf_drv_twi_t *twi_handle, uint8_t address, uint8_t sub_address)
-{   
-		ret_code_t err_code;
-		
-		err_code = nrf_drv_twi_tx(twi_handle, address, &sub_address, sizeof(sub_address), true);  
-		APP_ERROR_CHECK(err_code);
+	
+		while (m_xfer_done == false);
 	
 		return err_code;
 }
-    
-char read_byte(const nrf_drv_twi_t *twi_handle, uint8_t address, uint8_t sub_address)
+
+//reading byte or bytes from register            
+ret_code_t i2c_write_forread_byte(const nrf_drv_twi_t *twi_handle, uint8_t address, uint8_t sub_address)
 {   
 		ret_code_t err_code;
-		uint8_t data; // `data` will store the register data     
 		
-		write_forread_byte(twi_handle, address, sub_address);
-		
-		err_code = nrf_drv_twi_rx(twi_handle, address, &data, sizeof(data));
+		m_xfer_done = false;
+	
+		err_code = nrf_drv_twi_tx(twi_handle, address, &sub_address, sizeof(sub_address), true);  
 		APP_ERROR_CHECK(err_code);
- 
-		return data; 
+	
+		while (m_xfer_done == false);
+	
+		return err_code;
 }
+
         
-void read_bytes(const nrf_drv_twi_t *twi_handle, uint8_t address, uint8_t sub_address, uint8_t * dest, uint8_t dest_count)
+ret_code_t i2c_read_bytes(const nrf_drv_twi_t *twi_handle, uint8_t address, uint8_t sub_address, uint8_t * dest, uint8_t dest_count)
 {   
 		ret_code_t err_code;  
 		
-		write_forread_byte(twi_handle, address, sub_address);
+		// Write address to read from first
+		err_code = i2c_write_forread_byte(twi_handle, address, sub_address);
 		
-		err_code = nrf_drv_twi_rx(twi_handle, address, dest, dest_count);
-		APP_ERROR_CHECK(err_code);
+	
+		if (NRF_SUCCESS == err_code)
+		{			
+			m_xfer_done = false;
+		
+			// Now we can actually read the data
+			err_code = nrf_drv_twi_rx(twi_handle, address, dest, dest_count);
+			APP_ERROR_CHECK(err_code);
+	
+			while (m_xfer_done == false);
+		}
+	
+		return err_code;
 }
 
 
@@ -184,27 +162,16 @@ int main(void)
 		NRF_LOG_INFO("i2c init");
 		NRF_LOG_FLUSH();
 	
-		ret_code_t err_code;
-
-		uint8_t addr8 = 0x00U;
-		nrf_delay_ms(100);
-
+		uint8_t data1[1] = 0x00;
+	
+		i2c_write_byte(&m_twi, ICM_20948_I2C_ADDRESS, 0x06, data1, true);
 	
     while (true)
     {
         nrf_delay_ms(100);
 				NRF_LOG_FLUSH();
-			
-				m_xfer_done = false;
-				err_code = nrf_drv_twi_tx(&m_twi, ICM_20948_I2C_ADDRESS, &addr8, sizeof(addr8), true);
-				while (m_xfer_done == false);
-			
-				if (NRF_SUCCESS == err_code)
-				{
-					err_code = nrf_drv_twi_rx(&m_twi, ICM_20948_I2C_ADDRESS, value, sizeof(value));
-				}
-				APP_ERROR_CHECK(err_code);				
-		
+
+				i2c_read_bytes(&m_twi, ICM_20948_I2C_ADDRESS, 0x33, data, 1); // GYRO X H register	
     }
 }
 
