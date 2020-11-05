7c11f57e2faeaeef7da58910b337d38cf88d3664
jonacappelle <jonacappelle@hotmail.com>
2020-11-05 11:49:02 +0100

    I2C in separate file

--- pca10040/blank/arm5_no_packs/usr_twi.c
+++ pca10040/blank/arm5_no_packs/usr_twi.c
@@ -1,1 +1,117 @@
 #include "usr_twi.h"
+
+/* TWI instance. */
+const nrf_drv_twi_t m_twi = NRF_DRV_TWI_INSTANCE(TWI_INSTANCE_ID);
+
+/* Indicates if operation on TWI has ended. */
+static volatile bool m_xfer_done = false;
+
+/* I2C Initialisation */
+/**
+ * @brief UART initialization.
+ */
+void twi_init (void)
+{
+    ret_code_t err_code;
+
+    const nrf_drv_twi_config_t twi_imu_config = {
+       .scl                = ARDUINO_SCL_PIN,		// PIN 27
+       .sda                = ARDUINO_SDA_PIN,		// PIN 26
+       .frequency          = NRF_DRV_TWI_FREQ_400K,
+       .interrupt_priority = APP_IRQ_PRIORITY_HIGH,
+       .clear_bus_init     = false
+    };
+
+    err_code = nrf_drv_twi_init(&m_twi, &twi_imu_config, twi_handler, NULL);
+    APP_ERROR_CHECK(err_code);
+
+    nrf_drv_twi_enable(&m_twi);
+}
+
+/**
+ * @brief TWI events handler.
+ */
+void twi_handler(nrf_drv_twi_evt_t const * p_event, void * p_context)
+{
+    switch (p_event->type)
+    {
+        case NRF_DRV_TWI_EVT_DONE:
+            if (p_event->xfer_desc.type == NRF_DRV_TWI_XFER_RX)
+            {
+                //data_handler(data[0]);
+            }
+            m_xfer_done = true;
+            break;
+				case NRF_DRV_TWI_EVT_ADDRESS_NACK:
+						NRF_LOG_INFO("Address NACK");
+						break;
+				case NRF_DRV_TWI_EVT_DATA_NACK:
+						NRF_LOG_INFO("Data NACK");
+						break;
+        default:
+            break;
+    }
+}
+
+
+
+/* Low level write I2C functionality */
+ret_code_t i2c_write_byte(const nrf_drv_twi_t *twi_handle, uint8_t address, uint8_t sub_address, const uint8_t* data, uint32_t len, bool stop)
+{   
+		ret_code_t err_code;
+		
+		const uint8_t buf_len = len+1; // Register address + number of bytes
+    uint8_t tx_buf[buf_len];
+
+    tx_buf[0] = sub_address;
+    
+    memcpy(tx_buf+1, data, len); // Shift the data to make place for subaddress
+	
+		m_xfer_done = false;
+	
+		err_code = nrf_drv_twi_tx(twi_handle, address, tx_buf, buf_len, stop);  
+		APP_ERROR_CHECK(err_code);
+	
+		while (m_xfer_done == false);
+	
+		return err_code;
+}
+
+/* reading byte or bytes from register before writing: special function*/        
+ret_code_t i2c_write_forread_byte(const nrf_drv_twi_t *twi_handle, uint8_t address, uint8_t sub_address)
+{   
+		ret_code_t err_code;
+		
+		m_xfer_done = false;
+	
+		err_code = nrf_drv_twi_tx(twi_handle, address, &sub_address, sizeof(sub_address), true);  
+		APP_ERROR_CHECK(err_code);
+	
+		while (m_xfer_done == false);
+	
+		return err_code;
+}
+
+/* Low level read I2C functionality */
+ret_code_t i2c_read_bytes(const nrf_drv_twi_t *twi_handle, uint8_t address, uint8_t sub_address, uint8_t * dest, uint8_t dest_count)
+{   
+		ret_code_t err_code;  
+		
+		// Write address to read from first
+		err_code = i2c_write_forread_byte(twi_handle, address, sub_address);
+		
+	
+		if (NRF_SUCCESS == err_code)
+		{			
+			m_xfer_done = false;
+		
+			// Now we can actually read the data
+			err_code = nrf_drv_twi_rx(twi_handle, address, dest, dest_count);
+			APP_ERROR_CHECK(err_code);
+	
+			while (m_xfer_done == false);
+		}
+	
+		return err_code;
+}
+
--- pca10040/blank/arm5_no_packs/usr_twi.h
+++ pca10040/blank/arm5_no_packs/usr_twi.h
@@ -1,6 +1,25 @@
 #ifndef _USR_TWI_H_
 #define _USR_TWI_H_
 
+#include <stdio.h>
+#include "boards.h"
+#include "app_util_platform.h"
+#include "app_error.h"
+#include "nrf_drv_twi.h"
+#include "nrf_delay.h"
+
+#include "nrf_log.h"
+#include "nrf_log_ctrl.h"
+#include "nrf_log_default_backends.h"
+
+/* TWI instance ID. */
+#define TWI_INSTANCE_ID     0
+
+void twi_init (void);
+void twi_handler(nrf_drv_twi_evt_t const * p_event, void * p_context);
+ret_code_t i2c_write_byte(const nrf_drv_twi_t *twi_handle, uint8_t address, uint8_t sub_address, const uint8_t* data, uint32_t len, bool stop);
+ret_code_t i2c_write_forread_byte(const nrf_drv_twi_t *twi_handle, uint8_t address, uint8_t sub_address);
+ret_code_t i2c_read_bytes(const nrf_drv_twi_t *twi_handle, uint8_t address, uint8_t sub_address, uint8_t * dest, uint8_t dest_count);
 
 
 
